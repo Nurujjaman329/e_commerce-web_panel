0c0fb143ac92601ad9545ea43ec52531637711d3
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-10-05 18:08:53 +0600

    add main screen

---
+++ e_commerce_web_panel/lib/views/screens/main_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Management'),
+      ),
+      sideBar: SideBar(items: [
+        AdminMenuItem(
+          title: 'Dashboard',
+          icon: Icons.dashboard,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Vendors',
+          icon: CupertinoIcons.person_3,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Withdrawl',
+          icon: CupertinoIcons.money_dollar,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Orders',
+          icon: CupertinoIcons.shopping_cart,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Categories',
+          icon: Icons.category,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Products',
+          icon: Icons.shop,
+          route: '/',
+        ),
+        AdminMenuItem(
+          title: 'Upload Banners',
+          icon: CupertinoIcons.add,
+          route: '/',
+        ),
+      ], selectedRoute: ''),
+      body: Text('Dashboard'),
+    );
+  }
+}
