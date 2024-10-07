bbe9dbb6bc42f8eca314b69f8db5342d583f35d6
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-10-07 11:10:32 +0600

    modify Ui

--- e_commerce_web_panel/lib/views/screens/side_bar_screens/categories_screen.dart
+++ e_commerce_web_panel/lib/views/screens/side_bar_screens/categories_screen.dart
@@ -3,18 +3,105 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   static const String routeName = '\CategoriesScreen';
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  uploadCategory() {
+    if (_formKey.currentState!.validate()) {
+      print('Good');
+    } else {
+      print('Bad');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: EdgeInsets.all(10.0),
-        child: Text(
-          'Categories',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
-          ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Categories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 36,
+                ),
+              ),
+            ),
+            Divider(),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 140,
+                    width: 140,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade500,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text('Category'),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: SizedBox(
+                    width: 150,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Category Name Must not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                        hintText: 'Enter Category Name',
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    onPressed: () {
+                      uploadCategory();
+                    },
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Upload Image',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
